12e00883337f1d6b21d0ab0b28a21bde357ca892
khlifi ayoub <89302405+ayoub-sys@users.noreply.github.com>
2022-06-17 11:40:19 +0100

    Add files via upload

---
+++ steganography/LSB/decoder.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/python3
+
+import numpy as np
+from PIL import Image
+
+def Decode(src):
+
+    img = Image.open(src, 'r')
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+    total_pixels = array.size//n
+
+    hidden_bits = ""
+    for p in range(total_pixels):
+        for q in range(0, 3):
+            hidden_bits += (bin(array[p][q])[2:][-1])
+
+    hidden_bits = [hidden_bits[i:i+8] for i in range(0, len(hidden_bits), 8)]
+
+    message = ""
+    for i in range(len(hidden_bits)):
+        if message[-5:] == "$t3g0":
+            break
+        else:
+            message += chr(int(hidden_bits[i], 2))
+    if "$t3g0" in message:
+        print("Hidden Message:", message[:-5])
+    else:
+        print("No Hidden Message Found")
+
+
+if __name__=="__main__":
+    Decode('/home/ayoub/pythonHacking/steganography/LSB/image1.png')
